3f64ac72356177550a1f5d90c9bd033079c55df7
PrissyCorrea <pris.pcd@gmail.com>
2022-12-19 13:28:52 -0300

    pipe operators

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from 'src/app/Animals';
 
 @Component({
   selector: 'app-list-render',
@@ -6,11 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
-  animals = [
-    {name: "Logan", type: "Bunny"},
-    {name: "Eevee", type: "Bunny"},
-    {name: "Potter", type: "Cat"},
-    {name: "Kira", type: "Cat"},
-    {name: "Misa", type: "Cat"}
+  animals: Animal[] = [
+    {name: "Logan", type: "Bunny", age: 3},
+    {name: "Eevee", type: "Bunny", age: 1},
+    {name: "Potter", type: "Cat", age: 2},
+    {name: "Kira", type: "Cat", age: 1},
+    {name: "Misa", type: "Cat", age: 1}
   ]
+
+  animalDetails = ''
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos.`
+  }
 }
