f01168492adb5937205bbf12bbf422bef2de6b5e
PrissyCorrea <pris.pcd@gmail.com>
2022-12-19 16:02:56 -0300

    angular router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//array de objetos onde coloco o path de cada rota
+const routes: Routes = [
+  { path: "", component: FirstComponentComponent},
+  {path: 'list', component: ListRenderComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], //as rotas começam do root/raiz
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
